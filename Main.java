bd7ec4f9a9b7a69679c8a89c238e14990b65d818
Ficos <yurongyou@sjtu.edu.cn>
2016-03-17 08:48:31 +0800

    Keep in sync.

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@
  * Created by youyurong on 16/3/17.
  *
  * @author YurongYou
- *         <p>
- *         This class is intended to accomplish the main procedure of compiling.
+ *
+ * This class is intended to accomplish the main procedure of compiling.
  */
 public class Main {
     public static void main(String[] args) {
