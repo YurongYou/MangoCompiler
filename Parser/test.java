4d186455269ca7d6c096df40fb8c535e5a46473c
Ficos <yurongyou@sjtu.edu.cn>
2016-03-17 08:25:25 +0800

    Creata a parser folder.

---
+++ Parser/test.java
@@ -0,0 +1,10 @@
+package Parser;
+
+/**
+ * Created by youyurong on 16/3/17.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("一点人生的经验");
+    }
+}
