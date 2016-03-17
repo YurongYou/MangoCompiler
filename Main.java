9853736cda9119bbbb6db074c00f45d2ac1bd86a
Ficos <yurongyou@sjtu.edu.cn>
2016-03-17 08:10:25 +0800

    Update the initial Main class.

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by youyurong on 16/3/17.
+ *
+ * @author YurongYou
+ *         <p>
+ *         This class is intended to accomplish the main procedure of compiling.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("一颗赛艇");
+    }
+}
